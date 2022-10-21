5f3e5db5d35d775394fb19aebec84306e0cf76c6
lolmo <Blossom12!>
2022-10-20 17:29:03 -0700

    update

--- count-txts.sh
+++ count-txts.sh
@@ -1,3 +1,17 @@
-         find technical > find-results.txt
-         grep ".txt" find-results.txt > grep-results.txt
-         wc grep-results.txt
+     find technical > find-results.txt
+     grep ".txt" find-results.txt > grep-results.txt
+     string=$1;
+     if [[ $string == *"plos" ]]
+     then
+       find technical/plos > plos-results.txt;
+       grep ".txt" plos-results.txt > plos-results.txt
+       wc plos-results.txt
+    fi
+     if [[ $string == *"biomed" ]]
+         then
+           find technical/biomed > biomed-results.txt;
+           grep ".txt" biomed-results.txt > biomed-results.txt
+           wc biomed-results.txt
+        fi
+
+     wc grep-results.txt
